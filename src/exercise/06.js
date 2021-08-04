55a43b5e67405b2689043f451a7e37385a3f630a
Alexandre Fernandes <alexandrefernandes.ciandt@sulamerica.com.br>
2021-08-04 17:12:57 -0300

    exercise 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -9,22 +9,39 @@ function UsernameForm({onSubmitUsername}) {
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  
+  // Hook to use error behavior
+  // const [error, setError] = React.useState(null);
 
-  // 🐨 add the onSubmit handler to the <form> below
+  const [username, setUsername] = React.useState('');
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  function handleSubmit(event) {
+    event.preventDefault();
+    onSubmitUsername(username);
+  }
+
+  function handleChange(event) {
+    const {value} = event.target;
+    const valueLowerCase = value.toLowerCase();
+
+    // Show error message if input has Upper Case
+    // const isLowerCase = value === valueLowerCase;
+    // setError(isLowerCase ? null : 'You must type lower case only');
+
+    setUsername(valueLowerCase);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" value={username} onChange={handleChange} />
       </div>
+
+      {/* Error message is displayed here, with button disabled if is an error */}
+      {/* <div role="alert">{error}</div> */}
+      {/* <button disabled={Boolean(error)} type="submit">Submit</button> */}
+
       <button type="submit">Submit</button>
     </form>
   )
